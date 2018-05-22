3a96a80693a8925c309271fb1b0fe962aa2ad6c2
TenpuraTaiki <sleeping.3.fairies@gmail.com>
2018-05-22 09:43:03 +0900

    start 3-6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @post_images =@user.post_images.page(params[:page])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
 end
